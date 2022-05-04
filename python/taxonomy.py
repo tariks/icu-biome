1b12a6cd9c72b56ed6dfa0483f30ab96e7eba1d5
tariks <tariksalameh@gmail.com>
2022-05-04 07:36:00 -0400

    corrected preprocessing

--- python/taxonomy.py
+++ python/taxonomy.py
@@ -8,18 +8,24 @@ from qiime2.plugins import taxa
 asv = Artifact.load('../qiime/table_stool.qza')
 tax = Artifact.load('../qiime/tax_curated.qza')
 # %%
-#filt_asv = pd.read_csv('./icu-biome/feature_tables/59N_ASV_rare6k_filt002p7.csv',index_col=0).columns.to_list()
-meta = pd.read_csv('../meta/59N_alpha.csv',index_col=0)
+meta = pd.read_csv('../meta/52_alpha.csv',index_col=0)
 # %%
-ftable = ft.actions.filter_features_conditionally(asv,.00002,.01).filtered_table
-ftable = ft.actions.filter_samples(ftable,1000).filtered_table
+#ftable = ft.actions.filter_features_conditionally(asv,.00002,.01).filtered_table
+ftable = ft.actions.filter_samples(asv,1000).filtered_table
 # %%
 biggenus = taxa.actions.collapse(ftable,tax,7).collapsed_table.view(pd.DataFrame)
-biggenus.to_csv('../feature_tables/59N_genus.csv')
+biggenus.to_csv('../feature_tables/52N_genus.csv')
 ftable = taxa.actions.collapse(ftable,tax,7).collapsed_table
 biggenus.shape
 
 
+# %%
+def freq(table):
+    # return taxa proportion of total 
+    return table.sum() / table.sum().sum()
+def freq2(table):
+    # return prevalence across cohort
+    return (table>0).astype(int).sum() / table.shape[0]
 # %%
 def parseTaxonomy(table,thres=60):
     # extracts genus and applies IDTaxa threshold
@@ -46,24 +52,24 @@ def parseTaxonomy(table,thres=60):
     return df.loc[:,df.sum()>0].astype(int)
 
 # %%
-x=parseTaxonomy(genus,thres=70)
+big = parseTaxonomy(biggenus,thres=70)
+print(big.shape)
+s = freq(big).sort_values(ascending=False)
+s2 = freq2(big).sort_values(ascending=False)
+for i in s.index:
+    print('{}\t{:.2f}\t{:.2f}'.format(i,s[i],s2[i]))
+# %%
+x=parseTaxonomy(biggenus,thres=70)
 # %%
 x.to_csv('../feature_tables/59N_genus_t70.csv')
 # %%
-x=x.loc[meta['Day']==1]
-x=x.loc[x.sum(axis=1)>100]
-x.to_csv('../feature_tables/51N_genus_t70.csv')
+x=x.loc[meta.index]
+print(x.shape)
 
 # %%
 from skbio.stats.composition import multiplicative_replacement
 # %%
+x.loc[:,s>.2].to_csv('../selbal/52N_genus_t70.csv')
 x[:]=multiplicative_replacement(x)
-x.to_csv('../feature_tables/51N_genus_t70_nozero.csv')
-# %%
-meta=meta.loc[x.index]
-meta.loc[meta['Day']==1,['APACHE','Age']].to_csv('../selbal/covariates.csv')
-meta.loc[meta['Day']==1,'month'].astype(int).to_csv('../selbal/target.csv')
-# %%
-meta.to_csv('../meta/51N_alpha.csv')
-
+x.loc[:,s>.2].to_csv('../selbal/52N_genus_t70_nozero.csv')
 # %%
