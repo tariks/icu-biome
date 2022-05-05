2cfc28aa7182b63d40a54b5a62b875b7c32def60
tariks <tariksalameh@gmail.com>
2022-05-05 04:02:14 -0400

    selbal results

--- python/taxonomy.py
+++ python/taxonomy.py
@@ -10,14 +10,12 @@ tax = Artifact.load('../qiime/tax_curated.qza')
 # %%
 meta = pd.read_csv('../meta/52_alpha.csv',index_col=0)
 # %%
-#ftable = ft.actions.filter_features_conditionally(asv,.00002,.01).filtered_table
+#ftable = ft.actions.filter_features_conditionally(asv,.001,.01).filtered_table
 ftable = ft.actions.filter_samples(asv,1000).filtered_table
 # %%
-biggenus = taxa.actions.collapse(ftable,tax,7).collapsed_table.view(pd.DataFrame)
-biggenus.to_csv('../feature_tables/52N_genus.csv')
 ftable = taxa.actions.collapse(ftable,tax,7).collapsed_table
-biggenus.shape
-
+ftable = ftable.view(pd.DataFrame)
+ftable.shape
 
 # %%
 def freq(table):
@@ -26,50 +24,64 @@ def freq(table):
 def freq2(table):
     # return prevalence across cohort
     return (table>0).astype(int).sum() / table.shape[0]
+def freqprint(table):
+    s1,s2 = freq(table), freq2(table)
+    s2 = s2.sort_values()[::-1]
+    for i in s1.index:
+        print('{}\t{:.2f}\t{:.2f}'.format(i,s1[i],s2[i]))
+    return s2
 # %%
+import re
 def parseTaxonomy(table,thres=60):
     # extracts genus and applies IDTaxa threshold
-    # returns clean matrix
     df = pd.DataFrame(index=table.index)
-    cols=[]
-    for i in table.columns:
-        if 'genus' in i or 'Enterobacteriaceae' in i:
-            cols.append(i)
-    x=table[cols].copy()
+    cols=table.columns.to_list()
+    cols = [i for i in cols if '%' in i]
+    cols = [i for i in cols if 'genus' in i or 'Enterobacteriaceae' in i]
     for i in cols:
-        s = i.split(');')
-        if '__' in i:
-            s=s[-2]
-        else:
-            s=s[-1]
-        name = s.split(' (')[0]
-        s = s.split(' ')
-        if int(s[-1].split('%')[0]) > thres:
+        s = i.split(';')
+        for j in s:
+            if 'Enterobacteriaceae' in j:
+                s = j
+                break
+            elif 'genus' in j:
+                s = j
+        name = re.split('\s\(',s)[0]
+        score = int(re.findall('\d+%',s)[0][:-1])
+        if score > thres:
             if name in df.columns:
                 df[name]+=table[i]
             else:
                 df[name]=table[i].copy()
-    return df.loc[:,df.sum()>0].astype(int)
+    df = df.loc[:,(df.sum()>0)]
+    return df.astype(int)
 
 # %%
-big = parseTaxonomy(biggenus,thres=70)
+big = parseTaxonomy(ftable,thres=80)
+big=big.loc[meta.index]
+s2 = freqprint(big).sort_values(ascending=False)
 print(big.shape)
-s = freq(big).sort_values(ascending=False)
-s2 = freq2(big).sort_values(ascending=False)
-for i in s.index:
-    print('{}\t{:.2f}\t{:.2f}'.format(i,s[i],s2[i]))
 # %%
-x=parseTaxonomy(biggenus,thres=70)
-# %%
-x.to_csv('../feature_tables/59N_genus_t70.csv')
-# %%
-x=x.loc[meta.index]
-print(x.shape)
+# preview matrix
+x=big.loc[meta.index]
+print('Prevalence:')
+for i in [.2,.25,.3]:
+    print('>{}\t{}'.format(str(i)[1:],(s2>i).sum()))
+print('Samples < 5:')
+for i in [.2,.25,.3]:
+    #cnt = (x.loc[:,s2>i].sum(axis=1) < 1000).sum()
+    cnt = ((x.loc[:,s2>i] > 0).sum(axis=1) < 10).sum()
+    print('>{}\t{}'.format(str(i)[1:],cnt))
+
 
 # %%
 from skbio.stats.composition import multiplicative_replacement
 # %%
-x.loc[:,s>.2].to_csv('../selbal/52N_genus_t70.csv')
+idx=(s2>.2)
+idxrow = ( (x.loc[:,idx]>0).sum(axis=1) >=0)
+x.loc[idxrow,idx].to_csv('../feature_tables/52N_genus_t70.csv')
 x[:]=multiplicative_replacement(x)
-x.loc[:,s>.2].to_csv('../selbal/52N_genus_t70_nozero.csv')
+x.loc[idxrow,idx].to_csv('../feature_tables/52N_genus_t70_nozero.csv')
+meta.loc[idxrow,'month'].to_csv('../selbal/target.csv')
+meta.loc[idxrow,['APACHE','Age']].to_csv('../selbal/covariates.csv')
 # %%
